bfa7079af0e8792d1a5fdd9d8a08468f2ac33f2f
Gonzalo Barba <gonzalo@Gonzalos-MacBook-Pro.local>
2023-04-07 16:40:56 +0200

    add tailwindcss and styling

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,61 @@
+<script>
+    import "../app.css";
+</script>
+
+<!--
+  This example requires updating your template:
+
+  ```
+  <html class="h-full">
+  <body class="h-full">
+  ```
+-->
+<div class="min-h-full">
+    <nav class="border-b border-gray-200 bg-white">
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div class="flex h-16 justify-between">
+                <div class="flex">
+                    <div class="flex flex-shrink-0 items-center">
+                        <h1 class="text-gray-900 text-medium font-medium">
+                            🔔 Lluvia
+                        </h1>
+                    </div>
+                </div>
+                <div class="hidden sm:ml-6 sm:flex sm:items-center">
+                    <a href="mailto:me@gonzalobarba.com" type="button" class="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                        <span class="sr-only">View notifications</span>
+                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                            <path d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" stroke-linecap="round" stroke-linejoin="round"></path>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <div class="py-10">
+        <header>
+            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mb-4 sm:mb-8">
+                <h1 class="text-3xl font-bold leading-tight tracking-tight text-gray-900">
+                    Set your sound
+                </h1>
+            </div>
+        </header>
+        <main>
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <slot />
+            </div>
+        </main>
+        <footer aria-label="Site Footer" class="bg-white">
+            <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+                <div class="mt-8 border-t border-gray-100 pt-8">
+                    <div class="sm:flex sm:justify-between">
+                        <p class="text-xs text-gray-500">
+                            &copy; 2022. Gonzalo Barba. All rights reserved.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,21 +9,25 @@
     const data = [
         {
             name: '🌧️',
+            friendlyName: 'Heavy Rain',
             src: heavyRain,
             isPlaying: false
         },
         {
             name: '🚗',
+            friendlyName: 'Rain from a Car',
             src: rainFromACar,
             isPlaying: false
         },
         {
             name: '🏖️',
+            friendlyName: 'Rain on a Beach',
             src: rainBeach,
             isPlaying: false
         },
         {
             name: '🌳',
+            friendlyName: 'Rain in a Forest',
             src: rainForest,
             isPlaying: false
         }
@@ -65,14 +69,43 @@
     export { data };
 </script>
 
-<h1>Lluvia app</h1>
-<div>
+<ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
     {#each data as item}
-        <div>
-            <span>{item.name}</span>
-            <audio src={item.src} id={item.name}></audio>
-            <button on:click={() => toggleAudio(item.name)}>{#if item.isPlaying}Pause{:else}Play{/if}</button>
-            <input type="range" min="0" max="100" value="50" on:input={() => setVolume(item.name)} />
-        </div>
+        <li class="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow">
+            <div class="flex flex-1 flex-col p-8">
+                <div class="mx-auto h-32 w-32 flex-shrink-0 flex items-center justify-center">
+                    <span class="text-8xl">{item.name}</span>
+                </div>
+                <h3 class="mt-6 text-sm font-medium text-gray-900">{item.friendlyName}</h3>
+                <dl class="mt-1 flex flex-grow flex-col justify-between">
+                    <dd class="text-sm text-gray-500">Paradigm Representative</dd>
+                    <dt class="sr-only">Role</dt>
+                    <dd class="mt-3">
+                        <label for="default-range" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            🔊
+                            <span class="rounded-full bg-green-100 px-2 py-1 text-xs font-medium text-green-800">50%</span>
+                        </label>
+                        <input id="default-range" type="range"
+                               class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+                               min="0" max="100" value="50" on:input={() => setVolume(item.name)}>
+                    </dd>
+                </dl>
+            </div>
+            <div>
+                <div class="-mt-px flex divide-x divide-gray-200">
+                    <div class="-ml-px flex w-0 flex-1">
+                        <button on:click={() => toggleAudio(item.name)}
+                           class="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900">
+                            <svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor"
+                                 aria-hidden="true">
+                                <path d="M21 7.5V18M15 7.5V18M3 16.811V8.69c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 010 1.954l-7.108 4.061A1.125 1.125 0 013 16.811z"
+                                      stroke-linecap="round" stroke-linejoin="round"></path>
+                            </svg>
+                            {#if item.isPlaying}Pause{:else}Play{/if}
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </li>
     {/each}
-</div>
+</ul>
