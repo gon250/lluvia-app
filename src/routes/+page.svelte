00e86b5877558246a96355e3a70ae49c1b627506
Gonzalo Barba <gonzalo@Gonzalos-MacBook-Pro.local>
2023-04-12 17:17:42 +0200

    fix range volume reactivity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -99,7 +99,7 @@
                         </label>
                         <input id="default-range" type="range"
                                class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
-                               min="0" max="100" value={item.volume} on:input={() => setVolume(item.name)}>
+                               min="0" max="100" bind:value={item.volume} on:input={() => setVolume(item.name)}>
                     </dd>
                 </dl>
             </div>
@@ -107,16 +107,17 @@
                 <div class="-mt-px flex divide-x divide-gray-200">
                     <div class="-ml-px flex w-0 flex-1">
                         <button on:click={() => toggleAudio(item.name)}
-                           class="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900">
-                            <svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor"
-                                 aria-hidden="true">
-                                <path d="M21 7.5V18M15 7.5V18M3 16.811V8.69c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 010 1.954l-7.108 4.061A1.125 1.125 0 013 16.811z"
-                                      stroke-linecap="round" stroke-linejoin="round"></path>
-                            </svg>
+                                class="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900">
                             {#if item.isPlaying}
-                                Pause
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
+                                </svg>
+                                <span>Pause</span>
                             {:else}
-                                Play
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
+                                </svg>
+                                <span>Play</span>
                             {/if}
                         </button>
                     </div>
